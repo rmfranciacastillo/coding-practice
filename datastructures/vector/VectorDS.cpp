8bd9dbcd1bfa02f45b6133bf98156246522e066a
renato <rmfranciacastillo@gmail.com>
2022-07-25 17:53:12 -0500

    Added: Starter

---
+++ datastructures/vector/VectorDS.cpp
@@ -0,0 +1,22 @@
+/*
+ *  Program: Vector Example
+ *  Description: A simple program to show how Vector works and it's components
+ *
+ */ 
+
+#include <iostream>
+#include <vector> 
+
+using std::vector;
+
+int main() {
+  
+  vector<int> v; 
+  
+  v.push_back(1);
+  
+  
+
+  return 0;
+}
+
